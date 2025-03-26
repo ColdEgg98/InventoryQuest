bce3cd6c7866c50c1a7537752199904d61152d6d
ColdEgg98 <leytriss@gmail.com>
2025-03-26 10:11:16 +0900

    <Feat> 완료 "Status Canvas"

--- Assets/Scripts/UIHandler.cs
+++ Assets/Scripts/UIHandler.cs
@@ -1,8 +1,54 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIHandler : MonoBehaviour
 {
+    [Header("Status")]
+    public Button statusBtn;
+    public Image statusScreen;
+    public Button backBtn;
 
+    [Header("Inventory")]
+    public Button inventoryBtn;
+    public GameObject inventoryScreen;
+    public Button invenBackBtn;
+
+    private void Start()
+    {
+        statusBtn.onClick.AddListener(OpenStatus);
+        backBtn.onClick.AddListener(CloseStatus);
+
+        inventoryBtn.onClick.AddListener(OpenInventory);
+        invenBackBtn.onClick.AddListener(CloseInventory);
+    }
+
+    private void OpenStatus()
+    {
+        statusScreen.gameObject.SetActive(true);
+        statusBtn.gameObject.SetActive(false);
+        inventoryBtn.gameObject.SetActive(false);
+    }
+
+    private void CloseStatus()
+    {
+        statusScreen.gameObject.SetActive(false);
+        statusBtn.gameObject.SetActive(true);
+        inventoryBtn.gameObject.SetActive(true);
+    }
+    private void OpenInventory()
+    {
+        inventoryScreen.gameObject.SetActive(true);
+        inventoryBtn.gameObject.SetActive(false);
+        statusBtn.gameObject.SetActive(false);
+    }
+
+    private void CloseInventory()
+    {
+        inventoryScreen.gameObject.SetActive(false);
+        inventoryBtn.gameObject.SetActive(true);
+        statusBtn.gameObject.SetActive(true);
+    }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,16 +9,24 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     [Header("BaseInfo")]
-    private Player player;
     public TextMeshProUGUI nameTxt;
     public TextMeshProUGUI levelTxt;
     public TextMeshProUGUI EXPTxt;
     public TextMeshProUGUI goldAmountTxt;
     public Slider slider;
 
+    [Header("Status")]
+    public TextMeshProUGUI attackTxt;
+    public TextMeshProUGUI defenceTxt;
+    public TextMeshProUGUI healthTxt;
+    public TextMeshProUGUI critTxt;
+
+    private Player player;
+
     private void Start()
     {
         UpdateUI();
+        UpdateStat();
     }
 
     private void UpdateUI()
@@ -29,5 +39,14 @@ public class UIManager : MonoBehaviour
         goldAmountTxt.text = player.stat.gold.ToString();
         slider.maxValue = player.stat.maxEXP;
         slider.value = player.stat.currentEXP;
+    }
+    private void UpdateStat()
+    {
+        player = GameManager.instance.player;
+
+        attackTxt.text = player.stat.attack.ToString();
+        defenceTxt.text = player.stat.defence.ToString();
+        healthTxt.text = player.stat.health.ToString();
+        critTxt.text = player.stat.crit.ToString();
     }
 }
